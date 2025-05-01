ac85f75dea7a6b89e383dd7aec8162e1fb21eb76
Wilber jose matus <wilberj@gmail.com>
2025-04-30 22:42:43 -0600

    profiles-ext/i-sadav#4289 cancelacion automatica de reservas con fecha automatica de vencimiento

---
+++ SystemConfig/ConfiguracionesDataBaseModel.cs
@@ -0,0 +1,113 @@
+using API.Controllers;
+using APPCORE;
+namespace APPCORE.SystemConfig
+{
+	public class Transactional_Configuraciones : EntityClass
+	{
+		[PrimaryKey(Identity = true)]
+		public int? Id_Configuracion { get; set; }
+		public string? Nombre { get; set; }
+		public string? Descripcion { get; set; }
+		public string? Valor { get; set; }
+		public string? Tipo_Configuracion { get; set; }
+		public Transactional_Configuraciones? GetConfig(String prop)
+		{
+			Nombre = prop;
+			return Find<Transactional_Configuraciones>();
+		}
+		public static Transactional_Configuraciones GetParam(ConfiguracionesThemeEnum prop, string defaultValor = "", ConfiguracionesTypeEnum TYPE = ConfiguracionesTypeEnum.THEME)
+		{
+
+			var find = new Transactional_Configuraciones
+			{
+				Nombre = prop.ToString(),
+			}.Find<Transactional_Configuraciones>();
+			if (find == null)
+			{
+				find = new Transactional_Configuraciones
+				{
+					Valor = defaultValor,
+					Descripcion = prop.ToString(),
+					Nombre = prop.ToString(),
+					Tipo_Configuracion = TYPE.ToString()
+				};
+				find.Save();
+			}
+			return find;
+		}
+		public List<Transactional_Configuraciones> GetTheme()
+		{
+			return Get<Transactional_Configuraciones>()
+				.Where(x => x.Tipo_Configuracion != null &&
+				 x.Tipo_Configuracion.Equals(ConfiguracionesTypeEnum.THEME.ToString())).ToList();
+		}
+		public List<Transactional_Configuraciones> GetTypeNumbers()
+		{
+			return Get<Transactional_Configuraciones>()
+				.Where(x => x.Tipo_Configuracion != null &&
+				 x.Tipo_Configuracion.Equals(ConfiguracionesTypeEnum.NUMBER.ToString())).ToList();
+		}
+
+		public List<Transactional_Configuraciones> GetGeneralData()
+		{
+			return Get<Transactional_Configuraciones>()
+			   .Where(x => x.Tipo_Configuracion != null &&
+				x.Tipo_Configuracion.Equals(ConfiguracionesTypeEnum.GENERAL_DATA.ToString())).ToList();
+		}
+
+		public object? UpdateConfig(string? identity)
+		{
+			if (!AuthNetCore.HavePermission(APPCORE.Security.Permissions.ADMIN_ACCESS.ToString(), identity))
+			{
+				throw new Exception("no tienes permisos para configurar la aplicación");
+			}
+			return this.Update();
+		}
+
+		internal int GetParamNumberTemplate()
+		{
+			return Convert.ToInt32(Find<Transactional_Configuraciones>(
+				FilterData.Equal("Nombre", ConfiguracionesThemeEnum.PARAM_NUMBER_TEMPLATE)
+			)?.Valor ?? "0");
+		}
+	}
+	public enum AppConfigurationList
+	{
+		SQLCredentials,
+		SQLCredentialsSeguiminento,
+		IAServices,
+		MettaApi,
+		XApi,
+		Smtp,
+		AutomaticReports
+	}
+
+	public enum ConfiguracionesTypeEnum
+	{
+		THEME, GENERAL_DATA, NUMBER, SELECT,
+        IMAGE
+    }
+    
+
+	public enum ConfiguracionesThemeEnum
+	{
+		TITULO, SUB_TITULO, NOMBRE_EMPRESA, LOGO_PRINCIPAL, LOGO, MEDIA_IMG_PATH,
+		VERSION, PARAM_NUMBER_TEMPLATE, MESSAGE_TEMPLATE, AUTOMATIC_SENDER_REPORT,
+        DESTINATARIOS_AUTOMATIC_SENDER_REPORT,
+        MEMBRETE_HEADER,
+        MEMBRETE_FOOTHER,
+        DOMAIN_URL,
+        //ESTAS SE ESTAN USANDO PARA PLANTILLAS DE MENSAJES DE META API
+        TEMPLATE_NAME,
+        TEMPLATE_IMAGE_HEADER,
+        BLACK_LIST
+    }
+
+	public class Config
+	{
+		public static SystemConfig SystemConfig()
+		{
+			return new SystemConfig();
+		}
+	}
+}
---
+++ SystemConfig/SystemConfig.cs
@@ -0,0 +1,103 @@
+using APPCORE.Services;
+using Microsoft.Extensions.Configuration;
+namespace APPCORE.SystemConfig
+{
+	public class SystemConfig
+	{
+		public SystemConfig()
+		{
+			configuraciones = new Transactional_Configuraciones().Get<Transactional_Configuraciones>();
+			TITULO = configuraciones.Find(c => c.Nombre != null &&
+				c.Nombre.Equals(ConfiguracionesThemeEnum.TITULO.ToString()))?.Valor ?? TITULO;
+			SUB_TITULO = configuraciones.Find(c => c.Nombre != null &&
+				c.Nombre.Equals(ConfiguracionesThemeEnum.SUB_TITULO.ToString()))?.Valor ?? SUB_TITULO;
+			NOMBRE_EMPRESA = configuraciones.Find(c => c.Nombre != null &&
+				c.Nombre.Equals(ConfiguracionesThemeEnum.NOMBRE_EMPRESA.ToString()))?.Valor ?? NOMBRE_EMPRESA;
+			LOGO_PRINCIPAL = configuraciones.Find(c => c.Nombre != null &&
+				c.Nombre.Equals(ConfiguracionesThemeEnum.LOGO_PRINCIPAL.ToString()))?.Valor ?? LOGO_PRINCIPAL;
+			MEDIA_IMG_PATH = configuraciones.Find(c => c.Nombre != null &&
+				c.Nombre.Equals(ConfiguracionesThemeEnum.MEDIA_IMG_PATH.ToString()))?.Valor ?? MEDIA_IMG_PATH;
+			VERSION = configuraciones.Find(c => c.Nombre != null &&
+				c.Nombre.Equals(ConfiguracionesThemeEnum.VERSION.ToString()))?.Valor ?? VERSION;
+			MEMBRETE_HEADER = configuraciones.Find(c => c.Nombre != null &&
+				c.Nombre.Equals(ConfiguracionesThemeEnum.MEMBRETE_HEADER.ToString()))?.Valor ?? VERSION;
+			MEMBRETE_FOOTHER = configuraciones.Find(c => c.Nombre != null &&
+				c.Nombre.Equals(ConfiguracionesThemeEnum.MEMBRETE_FOOTHER.ToString()))?.Valor ?? VERSION;
+
+		}
+		public string TITULO = "TEMPLATE";
+		public string SUB_TITULO = "Template";
+		public string NOMBRE_EMPRESA = "TEMPLATE";
+		public string LOGO_PRINCIPAL = "logo.png";
+		public string MEDIA_IMG_PATH = "/media/img/";
+		public string VERSION = "2024.07";
+		public string MEMBRETE_HEADER = "";
+		public string MEMBRETE_FOOTHER = "";
+		public List<Transactional_Configuraciones> configuraciones = new List<Transactional_Configuraciones>();
+
+		public static bool IsAutomaticCaseActive()
+		{
+			//TODO IMPLEMENTAR ESTE METODO
+			return true;
+		}
+		public static bool IsNotificationsActive()
+		{
+			//TODO IMPLEMENTAR ESTE METODO
+			return true;
+		}
+		public static bool IsMessagesActive()
+		{
+			//TODO IMPLEMENTAR ESTE METODO
+			return false;
+		}
+		public static bool IsWhatsAppActive()
+		{
+			//TODO IMPLEMENTAR ESTE METODO
+			return true;
+		}
+		public static bool IsQuestionnairesActive()
+		{
+			//TODO IMPLEMENTAR ESTE METODO
+			return false;
+		}
+		public static IConfigurationRoot AppConfiguration()
+		{
+			return new ConfigurationBuilder()
+					.SetBasePath(Directory.GetCurrentDirectory())
+					.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+					.Build();
+		}
+		public static IConfigurationSection AppConfigurationSection(string sectionName)
+		{
+			return new ConfigurationBuilder()
+					.SetBasePath(Directory.GetCurrentDirectory())
+					.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+					.Build().GetSection(sectionName);
+		}
+		public static string? AppConfigurationValue(AppConfigurationList sectionName, string value)
+		{
+			return new ConfigurationBuilder()
+					.SetBasePath(Directory.GetCurrentDirectory())
+					.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+					.Build().GetSection(sectionName.ToString())[value];
+		}
+
+		internal static MailConfig? GetSMTPDefaultConfig()
+		{
+			string? domain = AppConfigurationValue(AppConfigurationList.Smtp, "Domain");
+			string? user = AppConfigurationValue(AppConfigurationList.Smtp, "User");
+			string? password = AppConfigurationValue(AppConfigurationList.Smtp, "Password");
+			string? port = AppConfigurationValue(AppConfigurationList.Smtp, "Port");
+			return new MailConfig()
+			{
+				HOST = domain,
+				PASSWORD = password,
+				USERNAME = user,
+				PORT = port != null ? Convert.ToInt32(port) : null,
+				AutenticationType = AutenticationTypeEnum.BASIC
+			};
+		}
+	}
+
+
+}
