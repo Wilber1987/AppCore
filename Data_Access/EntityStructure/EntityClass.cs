a753f9dd30caf1a5d0415475b1456522e234dc79
Wilber jose matus <wilberj@gmail.com>
2025-09-22 13:48:34 -0600

    profiles-ext/i-sadav#4979 CORRECCIONES EN EL ENTITY STRUCTURE

--- Data_Access/EntityStructure/EntityClass.cs
+++ Data_Access/EntityStructure/EntityClass.cs
@@ -86,7 +86,7 @@ public abstract class EntityClass : TransactionalClass
 				filterData.AddRange(where_condition?.ToList() ?? []);
 			}
 			// Intenta obtener la entidad utilizando los filtros establecidos
-			var Data = SqlADOConexion.SQLM != null ? SqlADOConexion.SQLM.TakeObject<T>(this) : default(T);
+			var Data = MDataMapper != null ? MDataMapper.TakeObject<T>(this) : default(T);
 			// Retorna la entidad encontrada o null si no se encuentra
 			return Data;
 		}
@@ -103,7 +103,7 @@ public abstract class EntityClass : TransactionalClass
 			// Establece los filtros de datos de la entidad
 			filterData = where_condition?.ToList();
 			// Intenta obtener la entidad utilizando los filtros establecidos
-			var Data = SqlADOConexion.SQLM != null ? SqlADOConexion.SQLM.TakeObject<T>(this, "", true) : default(T);
+			var Data = MDataMapper != null ? MDataMapper.TakeObject<T>(this, "", true) : default(T);
 			// Retorna la entidad encontrada o null si no se encuentra
 			return Data;
 		}
